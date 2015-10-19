61153d30180eac43ddbab436a6c1808db16aa912
Karl D. Asmussen <mhd@herebedragons.dk>
2015-10-19 19:49:00 +0200

    skeleton

--- src/main.c
+++ src/main.c
@@ -1,6 +1,32 @@
+#define MAIN_C
 #include <stdlib.h>
 
-int main() {
+#include <utils.h>
+#include <mod.h>
+#include <ioutils.h>
+#include <debug.h>
+#include <sim.h>
+#include <elf.h>
+#undef MAIN_C
+
+int main(int argc, char *argv[]) {
     
-    exit(EXIT_SUCCESS);
+    config_file conf;
+    MIPS_t mips;
+
+    set_debugf(stderr);
+
+    if (3 != argc) usage(argv[0]);
+    
+    clear_config_file(&conf);
+    read_config_file(argv[1], &conf);
+    
+    clear_MIPS(&mips);
+    configure_MIPS(&mips, &conf, 640*KB);
+    
+    read_MIPS(&mips, argv[2]);    
+
+    report_status(&mips);
+
+    free_MIPS(&mips);
 }
