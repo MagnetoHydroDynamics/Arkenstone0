402c2d6be7447b2a5b52b2d1f1a0fa79b78bed3c
Karl D. Asmussen <mhd@herebedragons.dk>
2015-10-19 19:49:17 +0200

    refactoring with ioutils

--- src/mod.c
+++ src/mod.c
@@ -1,16 +1,13 @@
 
+#define MOD_C
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
 
-#define SIM_C
-#include <sim.h>
+#include <mod.h>
 #include <ioutils.h>
 #include <debug.h>
-
-interp_r interpret(MIPS_t *mips) { mips = mips; return CONTINUE; }
-interp_r cycle(MIPS_t *mips) { mips = mips; return CONTINUE; }
-interp_r stall(MIPS_t *mips, phase_t ph) { mips = mips; ph = ph; return CONTINUE; }
+#undef MOD_C
 
 CACHE_t *new_CACHE(size_t sets, size_t blocks, size_t words) {
 
@@ -26,6 +23,7 @@ CACHE_t *new_CACHE(size_t sets, size_t blocks, size_t words) {
     res->n_words = words;
 
     res->misses = 0;
+    res->hits = 0;
 
     if (NULL == (res->data = malloc(res->sz = sizeof(b32) * sets * blocks * words)))
         errorf("ERROR: new_CACHE - unable to allocate memory\nReason: %s", strerror(errno));
@@ -36,7 +34,11 @@ CACHE_t *new_CACHE(size_t sets, size_t blocks, size_t words) {
     return res;
 }
 
-void init_MIPS(MIPS_t *mips, config_file *conf, size_t buf) {
+void clear_MIPS(MIPS_t *mips) {
+    memset(mips, 0, sizeof(MIPS_t));
+}
+
+void configure_MIPS(MIPS_t *mips, config_file *conf, size_t buf) {
     
     debug("Initializing MIPS model. Memory size: %zu\n", buf);
 
@@ -74,4 +76,44 @@ void free_MIPS(MIPS_t *mips) {
             free(mips->l2cache->data);
         free(mips->l2cache);
     }
+}
+
+void report_status(MIPS_t *mips) {
+    printf("Executed %zu instruction(s).\n"
+           "%zu cycle(s) elapsed.\n"
+           "icache hits: %zu, misses: %zu.\n"
+           "dcache hits: %zu, misses: %zu.\n"
+           "l2cache hits: %zu, misses: %zu.\n"
+           "pc = 0x%x\n",
+
+            mips->inst_cnt,
+            mips->cycle_cnt,
+
+            mips->icache->hits,
+            mips->icache->misses,
+            mips->dcache->hits,
+            mips->dcache->misses,
+
+            mips->l2cache->hits,
+            mips->l2cache->misses,
+
+            mips->pc);
+    
+    for (const b6 *p = &report_regs[0]; *p != ZERO; p++)
+        printf("%s = 0x%x\n", reg_names[*p], mips->regs[*p]);
+}
+
+void read_MIPS(MIPS_t *mips, const char *fname) {
+
+    switch (elf_dump(fname, &(mips->pc), &(*mips->mem)[0], mips->mem_sz)) {
+        case ELF_ERROR_IO_ERROR        :
+            errorf("ERROR: read_MIPS - problems reading file `%s'.\n", fname);
+        case ELF_ERROR_OUT_OF_MEM      :
+            errorf("ERROR: read_MIPS - ELF file larger than allocated memory buffer.\n");
+        case ELF_ERROR_NOT_ELF         :
+            errorf("ERROR: read_MIPS - not an ELF file.\n");
+        case ELF_ERROR_NOT_SUPPORTED   :
+            errorf("ERROR: read_MIPS - unsupported ELF features detected.\n");
+    }
+
 }
