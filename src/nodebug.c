d6ac73f543ff7aff1d6ecc996ec85c23e39186b8
Karl D. Asmussen <mhd@herebedragons.dk>
2015-10-19 13:30:59 +0200

    minor edits

--- src/nodebug.c
+++ src/nodebug.c
@@ -1,7 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
 
 #define DEBUG_C
 #include <debug.h>
 
-void set_debug(const char *_) {}
-void set_debugf(FILE *_) {}
-void debug(const char *_, ...) {}
+void set_debug(const char *_) { _ = _; }
+void set_debugf(FILE *_) { _ = _; }
+void debug(const char *_, ...) { _ = _; }
