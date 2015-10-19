b681f40ccff2dae6cd88a0cec09c29459f1cff2c
Karl D. Asmussen <mhd@herebedragons.dk>
2015-10-20 00:38:42 +0200

    stuff

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #define MAIN_C
 #include <stdlib.h>
+#include <string.h>
 
 #include <utils.h>
 #include <mod.h>
@@ -7,6 +8,7 @@
 #include <debug.h>
 #include <sim.h>
 #include <elf.h>
+#include <mips32.h>
 #undef MAIN_C
 
 int main(int argc, char *argv[]) {
@@ -23,6 +25,7 @@ int main(int argc, char *argv[]) {
     
     clear_MIPS(&mips);
     configure_MIPS(&mips, &conf, 640*KB);
+    memset(&(*mips.mem)[0], 0, mips.mem_sz);
     
     read_MIPS(&mips, argv[2]);    
 
