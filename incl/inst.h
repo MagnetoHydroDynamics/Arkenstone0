9ac82090c98f9826bafbdc992a5ff4bfc74c4479
Karl D. Asmussen <mhd@herebedragons.dk>
2015-10-19 13:37:49 +0200

    things

---
+++ incl/inst.h
@@ -0,0 +1,11 @@
+
+#ifndef INST_H
+#define INST_H
+
+#include <sim.h>
+
+interp_r J_j() { return CONTINUE; }
+
+#endif //ndef INST_H
+
+
---
+++ src/inst.c
@@ -0,0 +1,7 @@
+
+#define INST_C
+#include <inst.h>
+#include <sim.h>
+
+interp_r J_j();
+
