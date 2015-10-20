79f64b6b92a5f2cfb613d53f851e5afcd77a2738
Karl D. Asmussen <mhd@herebedragons.dk>
2015-10-20 19:52:21 +0200

    commenting

--- incl/sim.h
+++ incl/sim.h
@@ -3,7 +3,9 @@
 #define SIM_H
 
 #include <mod.h>
+#include <mem.h>
 
+// Interpretation infrastructure
 interp_r interpret(MIPS_t*);
 interp_r cycle(MIPS_t*);
 interp_r stage_if(MIPS_t*);
@@ -12,9 +14,7 @@ interp_r stage_ex(MIPS_t*);
 interp_r stage_mem(MIPS_t*);
 interp_r stage_wb(MIPS_t*);
 
-interp_r stall(MIPS_t*);
-
-// NULL if cache miss
-b8 *cache_lookup(MIPS_t*, b32, access_t);
+// Create bubble
+void stall(MIPS_t*);
 
 #endif //ndef SIM_H
