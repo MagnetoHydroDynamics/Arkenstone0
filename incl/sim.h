442db07fda2312c7323cab800e662c4c8db55f31
Karl D. Asmussen <mhd@herebedragons.dk>
2015-10-20 10:05:44 +0200

    immediate field in if/id

--- incl/sim.h
+++ incl/sim.h
@@ -6,6 +6,15 @@
 
 interp_r interpret(MIPS_t*);
 interp_r cycle(MIPS_t*);
-interp_r stall(MIPS_t*, phase_t);
+interp_r stage_if(MIPS_t*);
+interp_r stage_id(MIPS_t*);
+interp_r stage_ex(MIPS_t*);
+interp_r stage_mem(MIPS_t*);
+interp_r stage_wb(MIPS_t*);
+
+interp_r stall(MIPS_t*);
+
+// NULL if cache miss
+b8 *cache_lookup(MIPS_t*, b32, access_t);
 
 #endif //ndef SIM_H
